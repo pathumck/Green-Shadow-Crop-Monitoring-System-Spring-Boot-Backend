18232dab7fe2ce49693e8c323cacc6dec5af22e4
pathumck <ckpathumck@gmail.com>
2024-11-02 20:39:41 +0530

    initial commit

---
+++ src/main/java/lk/ijse/greenshadow/GreenshadowApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.greenshadow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenshadowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenshadowApplication.class, args);
+    }
+
+}
