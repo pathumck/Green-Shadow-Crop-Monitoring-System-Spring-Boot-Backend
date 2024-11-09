25886797d03aa9888f8711498df0707a12b4bafa
pathumck <ckpathumck@gmail.com>
2024-11-09 12:51:19 +0530

    update success response handling

--- src/main/java/lk/ijse/greenshadow/controller/FieldController.java
+++ src/main/java/lk/ijse/greenshadow/controller/FieldController.java
@@ -5,7 +5,9 @@ import lk.ijse.greenshadow.service.FieldService;
 import lk.ijse.greenshadow.util.AppUtil;
 import lk.ijse.greenshadow.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,12 +18,12 @@ public class FieldController {
     @Autowired
     FieldService fieldService;
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseUtil saveField(@RequestPart("fieldCode") String fieldCode,
-                                  @RequestPart("name") String name,
-                                  @RequestPart("location") String location,
-                                  @RequestPart("size") String size,
-                                  @RequestPart("imageOne") MultipartFile imageOne,
-                                  @RequestPart("imageTwo") MultipartFile imageTwo) {
+    public ResponseEntity<ResponseUtil> saveField(@RequestPart("fieldCode") String fieldCode,
+                                    @RequestPart("name") String name,
+                                    @RequestPart("location") String location,
+                                    @RequestPart("size") String size,
+                                    @RequestPart("imageOne") MultipartFile imageOne,
+                                    @RequestPart("imageTwo") MultipartFile imageTwo) {
         try {
             byte[] imageOneBytes = imageOne.getBytes();
             byte[] imageTwoBytes = imageTwo.getBytes();
@@ -29,14 +31,14 @@ public class FieldController {
             String imageTwoBase64 = AppUtil.imageToBase64(imageTwoBytes);
 
             fieldService.saveField(new FieldDTO(fieldCode, name, location, size, imageOneBase64, imageTwoBase64));
-            return new ResponseUtil("Success", "Field Saved", null);
+            return new ResponseEntity<>(new ResponseUtil("Success", "Field Saved", null), HttpStatus.CREATED);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseUtil updateField(@RequestPart("fieldCode") String fieldCode,
+    public ResponseEntity<ResponseUtil> updateField(@RequestPart("fieldCode") String fieldCode,
                                   @RequestPart("name") String name,
                                   @RequestPart("location") String location,
                                   @RequestPart("size") String size,
@@ -49,16 +51,16 @@ public class FieldController {
             String imageTwoBase64 = AppUtil.imageToBase64(imageTwoBytes);
 
             fieldService.updateField(new FieldDTO(fieldCode, name, location, size, imageOneBase64, imageTwoBase64));
-            return new ResponseUtil("Success", "Field Updated", null);
+            return new ResponseEntity<>(new ResponseUtil("Success", "Field Updated", null), HttpStatus.NO_CONTENT);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @DeleteMapping("/{fieldCode}")
-    public ResponseUtil deleteField(@PathVariable("fieldCode") String fieldCode) {
+    public ResponseEntity<ResponseUtil> deleteField(@PathVariable("fieldCode") String fieldCode) {
         fieldService.deleteField(fieldCode);
-        return new ResponseUtil("Success", "Field Deleted", null);
+        return new ResponseEntity<>(new ResponseUtil("Success", "Field Deleted", null), HttpStatus.NO_CONTENT);
     }
 
     @GetMapping
