3cc97aa19b9e6cb1a90ba70cf4fa88ccc37542e1
pathumck <ckpathumck@gmail.com>
2024-12-07 02:35:21 +0530

    create delete user

--- src/main/java/lk/ijse/greenshadow/controller/UserController.java
+++ src/main/java/lk/ijse/greenshadow/controller/UserController.java
@@ -4,10 +4,7 @@ import lk.ijse.greenshadow.repo.UserRepo;
 import lk.ijse.greenshadow.service.UserService;
 import lk.ijse.greenshadow.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -18,5 +15,11 @@ public class UserController {
     @GetMapping
     public ResponseUtil getAllUsers() {
         return new ResponseUtil("Success", "Retrieved All Users", userService.getAllUsers());
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseUtil deleteUser(@PathVariable("userId") String userId) {
+        userService.deleteUser(userId);
+        return new ResponseUtil("Success", "Deleted User", null);
     }
 }
